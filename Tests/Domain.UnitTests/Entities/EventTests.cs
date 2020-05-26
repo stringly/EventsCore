5b3a7a038059d9c860b752c13f71f2682c7fe9bd
Benjamin Carter <bc8107@gmail.com>
2020-05-25 20:41:27 -0400

    Event Entity tests in Progress

---
+++ Tests/Domain.UnitTests/Entities/EventTests.cs
@@ -0,0 +1,35 @@
+﻿using EventsCore.Domain.Entities;
+using EventsCore.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace EventsCore.Domain.UnitTests.Entities
+{
+    public class EventTests
+    {
+        [Fact]
+        public void Event_Given_Valid_Values_Is_Valid()
+        {
+            // Arrange
+            string newTitle = "new title";
+            string newDescription = "new description";
+            DateTime eventStart = new DateTime(3000, 2, 1);
+            DateTime eventEnd = new DateTime(3000, 2, 2);
+            DateTime regStart = new DateTime(3000, 1, 1);
+            DateTime regEnd = new DateTime(3000, 1, 2);
+
+            EventDates eventDates = new EventDates(eventStart, eventEnd, regStart, regEnd);
+
+            // Act
+            Event e = new Event(newTitle, newDescription, eventDates);
+
+
+            // Assert
+            Assert.Equal("new title", e.Title);
+            Assert.Equal("new description",e.Description);
+            Assert.Equal(eventDates, e.Dates);
+        }
+    }
+}
